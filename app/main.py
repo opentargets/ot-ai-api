3f88322aeba6fc685d39e869d0a630543fb96fe0
Carlos Cruz <me@carcruzcast.com>
2024-10-29 10:21:15 +0000

    feat: intial fastapp

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
+
+
+
+
+@app.get("/")
 def read_root():
-    return {'message': 'Hello, World!'}
+    return {"message": "Hello, World!"}
