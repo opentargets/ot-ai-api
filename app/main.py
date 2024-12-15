f85e41532aac263626e1963d6edf5a9664b9d398
Carlos Cruz <me@carcruzcast.com>
2024-12-15 02:31:48 -0600

    feat: base app

--- app/main.py
+++ app/main.py
@@ -1,12 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
-
-
-
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+async def read_root():
+    return {"message": "Open targets AI API"}
+
+
+# Error habdler
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        return JSONResponse(
+            status_code=404,
+            content={
+                "error": "Endpoint not found",
+                "detail": f"The requested URL {request.url.path} was not found on the server",
+            },
+        )
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.detail},
+    )
